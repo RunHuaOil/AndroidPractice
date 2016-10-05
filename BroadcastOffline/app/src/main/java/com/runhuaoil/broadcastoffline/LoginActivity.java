cf02dceaaab693dc6d5b55ed6e32be85f1979844
RunHuaOil <crh799250413@gmail.com>
2016-10-05 20:19:15 +0800

    Add SharedPreferences

--- BroadcastOffline/app/src/main/java/com/runhuaoil/broadcastoffline/LoginActivity.java
+++ BroadcastOffline/app/src/main/java/com/runhuaoil/broadcastoffline/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.runhuaoil.broadcastoffline;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,17 +20,28 @@ public class LoginActivity extends BaseActivity {
     private EditText edit_account;
     private EditText edit_psw;
     private  String account,password;
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private CheckBox checkBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
 
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
 
         Button button_login = (Button) findViewById(R.id.login);
         edit_account = (EditText) findViewById(R.id.account);
         edit_psw = (EditText) findViewById(R.id.password);
+        checkBox = (CheckBox) findViewById(R.id.check_box);
 
-
+        Boolean isRemember = pref.getBoolean("isRemember", false);
+        if (isRemember){
+            checkBox.setChecked(true);
+            edit_account.setText( pref.getString("account","") );
+            edit_psw.setText( pref.getString("password","") );
+        }
 
 
         button_login.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +51,16 @@ public class LoginActivity extends BaseActivity {
                 password = edit_psw.getText().toString();
                 Log.d("Test",account + " " + password);
                 if (account.equals("admin") && password.equals("123456")){
+
+                    editor = pref.edit();
+                    if (checkBox.isChecked()){
+                        editor.putBoolean("isRemember",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    }else {
+                        editor.clear();
+                    }
+                    editor.commit();
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
 
                     startActivity(intent);
